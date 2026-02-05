9b644ed2f4a615a0b2756271693ccec1f0a41fab
sriharinaidu62 <2023ugec062@nitjsr.ac.in>
2026-02-05 19:22:28 +0530

    Update gpio_test.c

--- Task2/RISCV/Firmware/gpio_test.c
+++ Task2/RISCV/Firmware/gpio_test.c
@@ -1,1 +1,18 @@
+#define GPIO_ADDR 0x00400020
 
+extern void print_hex(unsigned int);
+extern void print_string(const char *);
+
+int main() {
+    volatile unsigned int *gpio =
+        (volatile unsigned int *) GPIO_ADDR;
+
+    *gpio = 0xA5;               // WRITE
+    unsigned int val = *gpio;   // READ
+
+    print_string("GPIO readback = ");
+    print_hex(val);
+    print_string("\n");
+
+    while (1);
+}
