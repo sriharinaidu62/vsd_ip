7af14002f0b44cbfcce649ade220441a546f6404
sriharinaidu62 <2023ugec062@nitjsr.ac.in>
2026-02-07 17:23:36 +0530

    Update top_timer_fpga.v

--- final_Task/timer_ip/rtl/top_timer_fpga.v
+++ final_Task/timer_ip/rtl/top_timer_fpga.v
@@ -1,1 +1,27 @@
+module top_timer_fpga (
+    input        clk,        // onboard clock
+    input        rst_n,        // reset button
+    output [3:0] led          // LEDs on board
+);
+
+    wire timeout;
+
+    final_vsd_timer u_timer (
+        .clk     (clk),
+        .resetn  (rst_n),
+
+        // Tie-off bus for now
+        .sel     (1'b1),
+        .we      (1'b0),
+        .addr    (32'h0),
+        .wdata   (32'h0),
+        .rdata   (),
+        .timeout (timeout)
+    );
+
+    // Observe timer on LED
+    assign led[0] = timeout;
+    assign led[3:1] = 3'b000;
+
+endmodule
 
